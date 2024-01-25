1b77f80a5cb9629586cad0e77ef34f888287fcc9
unknown <you@example.com>
2024-01-25 15:55:58 +0800

    input reversed + changes in input

--- reversed_input_as_output.py
+++ reversed_input_as_output.py
@@ -1,8 +1,9 @@
 # pseudocode
 
 # Enter number
-number = input()
+number = input("Please enter your desired number: ")
 
 # Reversed the input
+rev_number = number[::-1]
 
 # Print the reversed input as output
